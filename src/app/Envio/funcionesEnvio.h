9c4891b981cfc38bd080680fb9908cd8f9a89950
ferdpecto <fernando.dominguez@alumni.mondragon.edu>
2016-03-17 18:06:32 +0100

    Se añaden cabecera de funciones para envío y fuente simulando dicho envío por UART.

---
+++ src/app/Envio/funcionesEnvio.h
@@ -0,0 +1,42 @@
+/**
+ * @file    funcionesEnvio.h
+ * @brief   Declaración de las funciones que para realizar las tareas de comunicación
+ * @par		Objetivo:
+ *			Este header se enfoca a la portabilidad del código. Los nombres de las funciones
+ *			y su funcionalidad serán siempre las mismas independientemente del protocolo.
+ * @author  F. Domínguez
+ * @date    17/03/2015
+ * @version 1.0
+ */
+
+#ifndef FUNCIONESENVIO_H_
+#define FUNCIONESENVIO_H_
+
+/*****************************************************
+*                   MODULES USED                     *
+*****************************************************/
+
+/*****************************************************
+*               DEFINITIONS AND MACROS               *
+*****************************************************/
+
+/*****************************************************
+*              TYPEDEFS AND STRUCTURES               *
+*****************************************************/
+
+/*****************************************************
+*                 EXPORTED VARIABLES                 *
+*****************************************************/
+
+/*****************************************************
+*                  EXPORTED FUNCTIONS                *
+*****************************************************/
+void APP_FUNCIONESENVIO_initChannel(void);
+void APP_FUNCIONESENVIO_send(char * str_id, char * data);
+void APP_FUNCIONESENVIO_receive(char * data);
+
+/*****************************************************
+*                        EOF                         *
+*****************************************************/
+
+#endif /* FUNCIONESENVIO_H_ */
---
+++ src/app/Envio/funcionesEnvioUART.c
@@ -0,0 +1,58 @@
+/**
+ * @file    funcionesEnvioUART.c
+ * @brief   Source con la implementación para mandar y recibir información por UART
+ * @par		Objetivo:
+ *			Se enfoca a la portabilidad del código. Los nombres de las funciones y su
+ *			funcionalidad serán siempre las mismas independientemente del protocolo.
+ *			En este caso se emplea el protocolo UART.
+ * @author  F. Domínguez
+ * @date    17/03/2015
+ * @version 1.0
+ */
+
+/*****************************************************
+*                   MODULES USED                     *
+*****************************************************/
+#include "funcionesEnvio.h"
+#include "platform.h"
+/*****************************************************
+*               DEFINITIONS AND MACROS               *
+*****************************************************/
+
+/*****************************************************
+*              TYPEDEFS AND STRUCTURES               *
+*****************************************************/
+
+/*****************************************************
+*           PROTOTYPES OF LOCAL FUNCTIONS            *
+*****************************************************/
+
+/*****************************************************
+*                EXPORTED VARIABLES                  *
+*****************************************************/
+
+/*****************************************************
+*                  GLOBAL VARIABLES                  *
+*****************************************************/
+
+/*****************************************************
+*                EXPORTED FUNCTIONS                  *
+*****************************************************/
+void APP_FUNCIONESENVIO_initChannel(void){
+	init_platform();
+}
+
+void APP_FUNCIONESENVIO_send(char * str_id, char * data){
+
+	xil_printf(str_id);
+	xil_printf(": ");
+	xil_printf(data);
+	xil_printf("\r\n");
+}
+void APP_FUNCIONESENVIO_receive(char * data){
+
+}
+
+/*****************************************************
+*                  LOCAL FUNCTIONS                   *
+*****************************************************/
