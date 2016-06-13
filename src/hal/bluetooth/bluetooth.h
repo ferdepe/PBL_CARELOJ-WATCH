7301c30d2c37628d31fdf3f50ea33d588b9b2e9a
javobarra <javobarra@gmail.com>
2016-06-13 15:59:36 +0200

    Agregado de subcarpetas en HAL: -Bluetooth -Memoria -GPS

---
+++ src/hal/bluetooth/bluetooth.h
@@ -0,0 +1,37 @@
+/**
+ * @file      bluetooth.h
+ * @brief     Cabecera de las funciones del bluetooth.
+ * @par		  Descripción de la función:
+ *			  Cabecera con la definición de las funciones
+ *			  generales públicas del bluetooth.
+ * @author    Javier Barragán
+ * @date      13/06/2016
+ * @version   1.0
+ * @todo	  desarrollar la librería
+ * @bug
+ */
+#ifndef BLUETOOTH_H_
+#define BLUETOOTH_H_
+
+
+
+/*****************************************************
+*                   MODULES USED                     *
+*****************************************************/
+
+/*****************************************************
+*               DEFINITIONS AND MACROS               *
+*****************************************************/
+
+/*****************************************************
+*              TYPEDEFS AND STRUCTURES               *
+*****************************************************/
+
+/*****************************************************
+*                 EXPORTED VARIABLES                 *
+*****************************************************/
+
+/*****************************************************
+*                  EXPORTED FUNCTIONS                *
+*****************************************************/
+#endif /* BLUETOOTH_H_ */
---
+++ src/hal/memoria/memoria.h
@@ -0,0 +1,36 @@
+/**
+ * @file      memoria.h
+ * @brief     Cabecera de las funciones del memoria.
+ * @par		  Descripción de la función:
+ *			  Cabecera con la definición de las funciones
+ *			  generales públicas del memoria.
+ * @author    Javier Barragán
+ * @date      13/06/2016
+ * @version   1.0
+ * @todo	  desarrollar la librería
+ * @bug
+ */
+
+#ifndef MEMORIA_H_
+#define MEMORIA_H_
+/*****************************************************
+*                   MODULES USED                     *
+*****************************************************/
+
+/*****************************************************
+*               DEFINITIONS AND MACROS               *
+*****************************************************/
+
+/*****************************************************
+*              TYPEDEFS AND STRUCTURES               *
+*****************************************************/
+
+/*****************************************************
+*                 EXPORTED VARIABLES                 *
+*****************************************************/
+
+/*****************************************************
+*                  EXPORTED FUNCTIONS                *
+*****************************************************/
+
+#endif /* MEMORIA_H_ */
